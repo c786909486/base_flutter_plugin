1f4307a1f06e96bcf9c042434ff3d0c85d44d20f
kzcai <ckz@axun.com.cn>
2022-03-11 17:45:45 +0800

    Default Changelist

--- lib/src/widget/common_text.dart
+++ lib/src/widget/common_text.dart
@@ -6,13 +6,19 @@ import 'package:base_flutter/base_flutter.dart';
 
 Text CommonText(String? text,
     {Color textColor = Colors.black,
-      double textSize = 16,
-      String hintText = "",
-      Color hintColor = Colors.grey,
-      TextAlign textAlign = TextAlign.start,FontWeight fontWeight = FontWeight.normal}) {
+    double textSize = 16,
+    String hintText = "",
+    Color hintColor = Colors.grey,
+    TextAlign textAlign = TextAlign.start,
+    double height = 1,
+    FontWeight fontWeight = FontWeight.normal}) {
   return Text(
-    text.isNullOrEmpty()?hintText:text??"",
+    text.isNullOrEmpty() ? hintText : text ?? "",
     textAlign: textAlign,
-    style: TextStyle(color: text.isNullOrEmpty()?hintColor:textColor, fontSize: textSize,fontWeight: fontWeight),
+    style: TextStyle(
+        color: text.isNullOrEmpty() ? hintColor : textColor,
+        fontSize: textSize,
+        height: height,
+        fontWeight: fontWeight),
   );
 }
