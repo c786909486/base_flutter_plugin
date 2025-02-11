6cbfc854c34f4c2c426bdb83a2ce6ddcbc66d110
ckz <18869978285@163.com>
2025-02-11 20:36:10 +0800

    加载弹窗默认不关闭

--- lib/src/base_mvvm/base_stateful_widget_mvvm.dart
+++ lib/src/base_mvvm/base_stateful_widget_mvvm.dart
@@ -256,7 +256,7 @@ abstract class BaseMvvmState<M extends BaseViewModel,
     }
   }
 
-  bool get touchOutDismiss => true;
+  bool get touchOutDismiss => false;
 
   bool get backDismiss => true;
 
