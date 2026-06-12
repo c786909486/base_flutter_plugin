1a9458aa9a9413bf20cd664255a4c298fc189ef5
ckz <18869978285@163.com>
2026-06-12 15:24:25 +0800

    fix:imageLoad增加请求头

--- lib/src/widget/image_loader_widget.dart
+++ lib/src/widget/image_loader_widget.dart
@@ -4,10 +4,12 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 
 typedef ImageCookieInit = String Function();
-class ImageLoad extends StatelessWidget {
+typedef ImageHeader = Map<String, String>? Function();
 
+class ImageLoad extends StatelessWidget {
   final String path;
   static ImageCookieInit? cookieInit;
+  static ImageHeader? headerInit;
   final ImageFrameBuilder? frameBuilder;
   final ImageLoadingBuilder? loadingBuilder;
   final String? errorImage;
@@ -31,53 +33,68 @@ class ImageLoad extends StatelessWidget {
   final String? package;
   Map<String, String>? headers;
 
-  ImageLoad(
-    this.path, {
-    this.frameBuilder,
-    this.loadingBuilder,
-    this.errorImage,
-    this.semanticLabel,
-    this.excludeFromSemantics = false,
-    this.width,
-    this.height,
-    this.color,
-    this.colorBlendMode,
-    this.fit,
-    this.alignment = Alignment.center,
-    this.repeat = ImageRepeat.noRepeat,
-    this.centerSlice,
-    this.matchTextDirection = false,
-    this.gaplessPlayback = false,
-    this.filterQuality = FilterQuality.low,
-    this.isAntiAlias = false,
-    this.scale = 1.0,
-        this.placeholder,
-        this.isAsset = false,
-        this.package,
-        this.headers
-  });
+  ImageLoad(this.path,
+      {this.frameBuilder,
+      this.loadingBuilder,
+      this.errorImage,
+      this.semanticLabel,
+      this.excludeFromSemantics = false,
+      this.width,
+      this.height,
+      this.color,
+      this.colorBlendMode,
+      this.fit,
+      this.alignment = Alignment.center,
+      this.repeat = ImageRepeat.noRepeat,
+      this.centerSlice,
+      this.matchTextDirection = false,
+      this.gaplessPlayback = false,
+      this.filterQuality = FilterQuality.low,
+      this.isAntiAlias = false,
+      this.scale = 1.0,
+      this.placeholder,
+      this.isAsset = false,
+      this.package,
+      this.headers});
 
   @override
   Widget build(BuildContext context) {
-    if(cookieInit!=null){
+    if (headerInit != null) {
+      var headerMap = headerInit!();
+      if (headers == null) {
+        headers = headerMap;
+      } else {
+        headers!.addAll(headerMap ?? {});
+      }
+    }
+    if (cookieInit != null) {
       String cookie = cookieInit!();
-      if(headers==null){
-        headers = {"Cookie":cookie};
-      }else{
+      if (headers == null) {
+        headers = {"Cookie": cookie};
+      } else {
         headers!['Cookie'] = cookie;
       }
     }
     return path.startsWith("http") || path.startsWith("https")
         ? Image(
-            image: CachedNetworkImageProvider(path, scale: scale,headers: headers),
+            image: CachedNetworkImageProvider(path,
+                scale: scale, headers: headers),
             frameBuilder: frameBuilder,
-            loadingBuilder: loadingBuilder!=null?loadingBuilder:placeholder.isNullOrEmpty()?null:(context,child,process){
-              if(process==null){
-                return child;
-              }else{
-                return Image.asset(placeholder??"",width: width,height: height,);
-              }
-            },
+            loadingBuilder: loadingBuilder != null
+                ? loadingBuilder
+                : placeholder.isNullOrEmpty()
+                    ? null
+                    : (context, child, process) {
+                        if (process == null) {
+                          return child;
+                        } else {
+                          return Image.asset(
+                            placeholder ?? "",
+                            width: width,
+                            height: height,
+                          );
+                        }
+                      },
             errorBuilder: (
               context,
               error,
@@ -110,71 +127,75 @@ class ImageLoad extends StatelessWidget {
             filterQuality: filterQuality,
             isAntiAlias: isAntiAlias,
           )
-        : isAsset?Image.asset(path,scale: scale, frameBuilder: frameBuilder,
-        package: package,
-        errorBuilder: (
-            context,
-            error,
-            stackTrace,
-            ) {
-          return errorImage.isNullOrEmpty()
-              ? Container(
-            width: width,
-            height: height,
-          )
-              : Image.asset(
-            errorImage!,
-            width: width,
-            height: height,
-          );
-        },
-        semanticLabel: semanticLabel,
-        excludeFromSemantics: excludeFromSemantics,
-        width: width,
-        height: height,
-        color: color,
-        colorBlendMode: colorBlendMode,
-        fit: fit,
-        alignment: alignment,
-        repeat: repeat,
-        centerSlice: centerSlice,
-        matchTextDirection: matchTextDirection,
-        gaplessPlayback: gaplessPlayback,
-        filterQuality: filterQuality,
-        isAntiAlias: isAntiAlias):Image.file(new File(path), scale: scale, frameBuilder: frameBuilder,
-        errorBuilder: (
-            context,
-            error,
-            stackTrace,
-            ) {
-          return errorImage.isNullOrEmpty()
-              ? Container(
-            width: width,
-            height: height,
-          )
-              : Image.asset(
-            errorImage!,
-            width: width,
-            height: height,
-          );
-        },
-        semanticLabel: semanticLabel,
-        excludeFromSemantics: excludeFromSemantics,
-        width: width,
-        height: height,
-        color: color,
-        colorBlendMode: colorBlendMode,
-        fit: fit,
-        alignment: alignment,
-        repeat: repeat,
-        centerSlice: centerSlice,
-        matchTextDirection: matchTextDirection,
-        gaplessPlayback: gaplessPlayback,
-        filterQuality: filterQuality,
-        isAntiAlias: isAntiAlias);
+        : isAsset
+            ? Image.asset(path,
+                scale: scale,
+                frameBuilder: frameBuilder,
+                package: package, errorBuilder: (
+                context,
+                error,
+                stackTrace,
+              ) {
+                return errorImage.isNullOrEmpty()
+                    ? Container(
+                        width: width,
+                        height: height,
+                      )
+                    : Image.asset(
+                        errorImage!,
+                        width: width,
+                        height: height,
+                      );
+              },
+                semanticLabel: semanticLabel,
+                excludeFromSemantics: excludeFromSemantics,
+                width: width,
+                height: height,
+                color: color,
+                colorBlendMode: colorBlendMode,
+                fit: fit,
+                alignment: alignment,
+                repeat: repeat,
+                centerSlice: centerSlice,
+                matchTextDirection: matchTextDirection,
+                gaplessPlayback: gaplessPlayback,
+                filterQuality: filterQuality,
+                isAntiAlias: isAntiAlias)
+            : Image.file(new File(path),
+                scale: scale, frameBuilder: frameBuilder, errorBuilder: (
+                context,
+                error,
+                stackTrace,
+              ) {
+                return errorImage.isNullOrEmpty()
+                    ? Container(
+                        width: width,
+                        height: height,
+                      )
+                    : Image.asset(
+                        errorImage!,
+                        width: width,
+                        height: height,
+                      );
+              },
+                semanticLabel: semanticLabel,
+                excludeFromSemantics: excludeFromSemantics,
+                width: width,
+                height: height,
+                color: color,
+                colorBlendMode: colorBlendMode,
+                fit: fit,
+                alignment: alignment,
+                repeat: repeat,
+                centerSlice: centerSlice,
+                matchTextDirection: matchTextDirection,
+                gaplessPlayback: gaplessPlayback,
+                filterQuality: filterQuality,
+                isAntiAlias: isAntiAlias);
   }
 }
 
-String formatImage(String name, {String format = ".png",String parentPath = "images"}) {
+String formatImage(String name,
+    {String format = ".png", String parentPath = "images"}) {
   return "${parentPath}/$name$format";
 }
